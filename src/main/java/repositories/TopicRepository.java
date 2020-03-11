c729452d6e1e0212192e48e38b4ebc71ecf45854
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-03-11 15:48:19 -0400

    add topic repository and widget repository

---
+++ src/main/java/repositories/TopicRepository.java
@@ -0,0 +1,23 @@
+package repositories;
+
+import com.example.wbdvsp20jiawenlispringserver.models.Topic;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TopicRepository
+    extends CrudRepository<Topic, Integer> {
+
+    @Query(value = "SELECT * from topics", nativeQuery = true)
+    public List<Topic> findAllTopics();
+
+    @Query(value = "SELECT * from topics where topics.lessonId = :lid", nativeQuery = true)
+    public List<Topic> findTopicForLesson(@Param("lid") String lessonId);
+
+    @Query(value = "SELECT * from topics where topics.id = :tid", nativeQuery = true)
+    public Topic findTopicById(@Param("tid") Integer topicId);
+
+
+}
---
+++ src/main/java/repositories/WidgetRepository.java
@@ -0,0 +1,22 @@
+package repositories;
+
+import com.example.wbdvsp20jiawenlispringserver.models.Topic;
+import com.example.wbdvsp20jiawenlispringserver.models.Widget;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface WidgetRepository
+    extends CrudRepository<Widget, Integer> {
+
+    @Query(value = "SELECT * from widgets", nativeQuery = true)
+    public List<Widget> findAllWidgets();
+
+    @Query(value = "SELECT * from widgets where widgets.topic_id =:tid", nativeQuery = true)
+    public List<Topic> findWidgetForTopic(@Param("tid") Integer topicId);
+
+    @Query(value = "SELECT * from widgets where widgets.id =:wid", nativeQuery = true)
+    public Widget findWidgetById(@Param("wid") String widgetId);
+}
